dd61826bd8bc1f95a9732fcd11c94eaebd2c0aaa
18016624 <18016624@myrp.edu.sg>
2020-05-29 11:56:53 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demonotification/MainActivity.java
@@ -0,0 +1,103 @@
+package com.myapplicationdev.android.demonotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    int requestCode = 123;
+    int notificationID = 888;
+    Button btnNotify1, btnNotify2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnNotify1 = findViewById(R.id.btnNotify1);
+        btnNotify2 = findViewById(R.id.btnNotify2);
+
+        btnNotify1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationManager notificationManager = (NotificationManager)
+                        getSystemService(NOTIFICATION_SERVICE);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    NotificationChannel channel = new NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_HIGH);
+
+                    channel.setDescription("This is for default notification");
+                    notificationManager.createNotificationChannel(channel);
+                }
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pIntent = PendingIntent.getActivity(MainActivity.this, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "default");
+                builder.setContentTitle("Amazing Offer!");
+                builder.setContentText("Subject");
+                builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+                builder.setContentIntent(pIntent);
+                builder.setAutoCancel(true);
+
+                Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                builder.setSound(uri);
+
+                builder.setPriority(Notification.PRIORITY_HIGH);
+                Notification n = builder.build();
+
+                notificationManager.notify(notificationID, n);
+                finish();
+
+            }
+        });
+
+        btnNotify2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationManager notificationManager = (NotificationManager)
+                        getSystemService(NOTIFICATION_SERVICE);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    NotificationChannel channel = new NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+                    channel.setDescription("This is for default notification");
+                    notificationManager.createNotificationChannel(channel);
+                }
+                //WHEN CLICK ON NOTI GO WHICH PAGE IS DONE HERE
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                PendingIntent pIntent = PendingIntent.getActivity(MainActivity.this, requestCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
+                bigText.setBigContentTitle("Big Text - Long Content");
+                bigText.bigText("This is one big text" + " - A  quick brown fox jumps over a lazy brown dog "
+                        + "\n I am a boy and i love to eat, Ice cream very much");
+                bigText.setSummaryText("Reflection Journal");
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "default");
+                builder.setContentTitle("Amazing Offer!");
+                builder.setContentText("Big Text Subject");
+                builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+                builder.setContentIntent(pIntent);
+                builder.setStyle(bigText);
+                builder.setAutoCancel(true);
+
+                Notification n = builder.build();
+
+                notificationManager.notify(notificationID, n);
+                finish();
+            }
+        });
+    }
+}
